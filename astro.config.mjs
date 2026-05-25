95115c92683bfce4c8566d2c5e8e2df08841ccb5
Vladimir Vujosevic <vladimirvuj@gmail.com>
2026-05-26 01:23:01 +0200

    Revert repo to static Astro Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 const site = process.env.SITE_URL ?? process.env.PUBLIC_SITE_URL;
 
 // https://astro.build/config
 export default defineConfig({
   site,
   integrations: site ? [sitemap()] : [],
-  adapter: cloudflare(),
 });
